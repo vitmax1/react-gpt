aeede4d0a97c54b6010967db30db96e14e378862
Vitaly Maksimov <2california@mail.ru>
2023-03-03 16:00:30 +0600

    header finish

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -40,28 +40,20 @@ const Navbar = () => {
         <button type="button">Sign Up</button>
       </div>
       <div className="gpt3__navbar-menu">
-        {toggleMenu ? (
-          <RiCloseLine
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(false)}
-          />
-        ) : (
-          <RiMenu3Line
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(true)}
-          />
-        )}
-        {toggleMenu && (
-          <div className="gpt3__navbar-menu_container scale-up">
-            <Menu />
-            <div className="gpt3__navbar-menu_container-links-sign">
-              <p>Sign in</p>
-              <button type="button">Sign Up</button>
-            </div>
-          </div>
-        )}
+			{
+				toggleMenu ? ( <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>) : 
+				( <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>)
+			}
+			{
+				toggleMenu && (
+          	<div className="gpt3__navbar-menu_container scale-up">
+					<Menu />
+					<div className="gpt3__navbar-menu_container-links-sign">
+						<p>Sign in</p>
+						<button type="button">Sign Up</button>
+					</div>
+				</div>)
+			}
       </div>
     </div>
   );
